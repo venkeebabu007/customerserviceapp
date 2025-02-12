e8b34c8d2c42caf22dc8e49c2bd705ca0472ff25
venkeebabu007 <venkeebabu007@gmail.com>
2025-02-12 15:13:23 +0530

    middleware logic

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -35,12 +35,22 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  if (!user && !request.nextUrl.pathname.startsWith("/signin") && !request.nextUrl.pathname.startsWith("/auth")) {
+  if (user && request.nextUrl.pathname.startsWith("/admin")) {
+    const { data: userData } = await supabase.from("users_csapp").select("role").eq("auth_user_id", user.id).single()
+
+    if (userData?.role !== "admin") {
+      return NextResponse.redirect(new URL("/", request.url))
+    }
+  }
+
+  if (!user && !request.nextUrl.pathname.startsWith("/login") && !request.nextUrl.pathname.startsWith("/auth")) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
-    url.pathname = "/signin"
+    url.pathname = "/login"
     return NextResponse.redirect(url)
   }
+
+
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is.
   // If you're creating a new response object with NextResponse.next() make sure to:
